ec016e82c41fbb6a4321c5c9ea5c602ff20ef86d
Tc <jackgoodchen2019@gmail.com>
2020-03-22 07:37:42 +0800

    upd

--- router/urls.py
+++ router/urls.py
@@ -3,6 +3,7 @@
 from apps.sso import urls as sso_urls
 from apps.public import urls as public_urls
 from apps.user import urls as user_urls
+from apps.shop import urls as shop_urls
 
 urlpattern=[
     #
@@ -12,3 +13,4 @@ urlpattern=[
 urlpattern+=sso_urls.urlpattern
 urlpattern+=public_urls.urlpattern
 urlpattern+=user_urls.urlpattern
+urlpattern+=shop_urls.urlpattern

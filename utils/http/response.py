ab08e1560bf26d8406d0a054ed3abc1bbddf63bf
Tc <jackgoodchen2019@gmail.com>
2020-05-06 12:37:48 +0800

    upd

--- utils/http/response.py
+++ utils/http/response.py
@@ -1,5 +1,6 @@
 
 import json
+import decimal
 
 code = {
     'success': '10000',
@@ -10,6 +11,15 @@ res = {
     'data': '',
     'code': code['success'],
 }
+
+class DecimalEncoder(json.JSONEncoder):
+    def _iterencode(self, o, markers=None):
+        if isinstance(o, decimal.Decimal):
+            # wanted a simple yield str(o) in the next line,
+            # but that would mean a yield on the line with super(...),
+            # which wouldn't work (see my comment below), so...
+            return (str(o) for o in [o])
+        return super(DecimalEncoder, self)._iterencode(o, markers)
 
 def HttpResponse(self=None,success=True,data=None,rescode=code['success'],msg='',headers=None,count=None):
     # if self.status_code != 200:
@@ -27,4 +37,4 @@ def HttpResponse(self=None,success=True,data=None,rescode=code['success'],msg=''
             res['msg'] = msg if msg else '请求出错,请稍后再试！'
     res['data'] = data
     res['count'] = count
-    return json.dumps(res)
+    return json.dumps(res,cls=DecimalEncoder)

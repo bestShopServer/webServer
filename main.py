5b906f0561a1972b1a86a841e650fcfd47e8b192
Tc <jackgoodchen2019@gmail.com>
2020-07-01 23:33:04 +0800

    upd

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 
 
-from utils import configinit_setup
-from utils import Server
+
 import os
 import sys
 import django
-
 
 if __name__ == "__main__":
     pathname = os.path.dirname(os.path.abspath(__file__))
@@ -15,6 +13,9 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "utils.django.settings")
 
     django.setup()
+
+    from utils import configinit_setup
+    from utils import Server
 
     configinit_setup()
     Server().start()

9df1f14d164bb8df1687639787d128f2c7ea644c
Tc <jackgoodchen2019@gmail.com>
2020-07-04 13:03:44 +0800

    upd

--- apps/web/public/forms.py
+++ apps/web/public/forms.py
@@ -10,7 +10,7 @@ class AttachMentGroupForm(Form):
 
     id = IntegerField()
     userid = IntegerField("用户代码",validators=[DataRequired(message="请输入用户代码")])
-    name = StringField("分组名称", validators=[DataRequired(message="请输入分组名称"), Length(min=2,max=10, message="分组名称长度为2-10")])
+    name = StringField("分组名称", validators=[DataRequired(message="请输入分组名称"), Length(min=2,max=30, message="分组名称长度为2-30")])
     type = StringField("素材类型", validators=[DataRequired(message="请输入素材类型")])
 
     def validate_type(self,field):
@@ -22,7 +22,7 @@ class AttachMentForm(Form):
     id = IntegerField()
     grouid = IntegerField()
     userid = IntegerField("用户代码", validators=[DataRequired(message="请输入用户代码")])
-    name = StringField("素材名称", validators=[DataRequired(message="请输入素材名称"), Length(min=2,max=10, message="素材名称长度为2-30")])
+    name = StringField("素材名称", validators=[DataRequired(message="请输入素材名称"), Length(min=2,max=60, message="素材名称长度为2-60")])
     type = StringField("素材类型", validators=[DataRequired(message="请输入素材类型")])
     url = StringField("素材链接", validators=[DataRequired(message="请输入素材链接")])
 

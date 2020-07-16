52911aced2926f790bf19caa53768c158a96ba07
Tc <jackgoodchen2019@gmail.com>
2020-07-16 22:07:45 +0800

    upd

--- apps/web/shop/rule.py
+++ apps/web/shop/rule.py
@@ -54,7 +54,7 @@ class ShopPageRules:
                     "page":True,
                     "serializers":ShopPageSerializer,
                     "detail_serializers":ShopPageDetailSerializer,
-                    "sort": [ShopPage.type,ShopPage.createtime.desc()],
+                    "sort": [ShopPage.createtime.desc(),ShopPage.type],
                     "query_params":[
                         {
                             "key":"userid",

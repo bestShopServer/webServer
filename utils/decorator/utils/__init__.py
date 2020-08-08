48f26e52c734e08b8b64ff622b4cd89bcca50099
Tc <jackgoodchen2019@gmail.com>
2020-08-09 01:19:50 +0800

    upd

--- utils/decorator/utils/__init__.py
+++ utils/decorator/utils/__init__.py
@@ -55,5 +55,6 @@ async def run_before_func_run(**kwargs):
         elif connector_app.request.method == 'DELETE':
             await ConnectorFuncsDeleteBase(**kwargs).run()
         elif connector_app.request.method == 'GET':
-            await connector.get_before_hander(connector_app,**kwargs)
+            if connector.get_before_hander:
+                await connector.get_before_hander(connector_app,**kwargs)
             return await ConnectorFuncsGetBase(**kwargs).run()

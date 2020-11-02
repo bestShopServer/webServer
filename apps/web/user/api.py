0205d576323fc3b8f8e890de237fece80e9dae82
Tc <tangchen2018@hotmail.com>
2020-11-02 17:41:31 +0800

    upd

--- apps/web/user/api.py
+++ apps/web/user/api.py
@@ -16,9 +16,8 @@ class userinfo(BaseHandler):
 
         return {"data": {
             "userid": self.user.userid,
-            "loginname": self.user.uuid,
             "username": self.user.name,
-            "rolecode":self.user.rolecode,
-            "avatar": 'http://allwin6666.com/nginx_upload/assets/login.jpg',
+            "rolecode":self.user.role_code,
+            "avatar": 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1604320145113&di=c0f37be5cc6331c65ec5773edbf7c1da&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201703%2F18%2F20170318012043_H4mRj.jpeg',
             "menu": []
         }}
